e728cd3d532d4cc9171ccd80d2a76ca8c6b5f3aa
Loren Hale <loren.hale@gmail.com>
2014-06-05 11:14:26 -0500

    bar initializes with empty menu

--- bar.rb
+++ bar.rb
@@ -1,9 +1,10 @@
 require 'time' # you're gonna need it
 
 class Bar
-  attr_reader :name
+  attr_reader :name, :menu_items
 
   def initialize(name)
-    @name = name
+    @name       = name
+    @menu_items = { }
   end
 end
