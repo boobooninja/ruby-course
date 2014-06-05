e31562c342733d0975ca33f5ca42b676fadd301b
Loren Hale <loren.hale@gmail.com>
2014-06-05 09:41:36 -0500

    Motorcycle initializes with 2 wheels and red color

--- car.rb
+++ car.rb
@@ -14,10 +14,15 @@ end
 class BigRig < Car
   def initialize(color, wheel_count=18)
     super
-    @wheel_count = wheel_count
   end
 
   def honk
     "BBBBBRRRRRRAAAAAWWWHHHHH"
+  end
+end
+
+class Motorcycle < Car
+  def initialize(color='red', wheel_count=2)
+    super
   end
 end
