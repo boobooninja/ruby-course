f363c56bc89088f218e34a5128837dbfb0448dbc
Loren Hale <loren.hale@gmail.com>
2014-06-05 10:44:43 -0500

    use inject

--- car_stats.rb
+++ car_stats.rb
@@ -1,29 +1,20 @@
 class CarStats
   def self.calc_top_color(cars)
-    car_hash = cars.each_with_object(Hash.new(0)) {|car,count| count[car.color] += 1}
-    top_color = nil
-    top_count = 0
-    car_hash.each do|color,count|
-      if count > top_count
-        top_count = count
-        top_color = color
-      end
-    end
-
-    top_color
+    car_hash(cars).max_by {|color,count| count}.first
   end
 
   def self.calc_bottom_color(cars)
-    car_hash = cars.each_with_object(Hash.new(0)) {|car,count| count[car.color] += 1}
-    bottom_color = nil
-    bottom_count = 100
-    car_hash.each do|color,count|
-      if count < bottom_count
-        bottom_count = count
-        bottom_color = color
-      end
-    end
+    car_hash(cars).min_by {|color,count| count}.first
+  end
 
-    bottom_color
+  def self.car_hash(cars)
+    # cars.each_with_object(Hash.new(0)) do |car,count|
+    #   count[car.color] += 1
+    # end
+
+    cars.inject(Hash.new(0)) do |hash, car|
+      hash[car.color] += 1
+      hash
+    end
   end
 end
