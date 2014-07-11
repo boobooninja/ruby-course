869c42386dfdb0e93573adabf344b954c63ad9d3
Loren Hale <loren.hale@gmail.com>
2014-07-10 21:02:45 -0500

    refactor sign_in_spec

--- spec/use_cases/sign_in_spec.rb
+++ spec/use_cases/sign_in_spec.rb
@@ -2,77 +2,71 @@ require 'spec_helper'
 
 describe DoubleDog::SignIn do
   describe 'validation' do
+    before(:each) do
+      @use_case = DoubleDog::SignIn.new
+      @args     = {username: 'valid_username', password: 'valid_password'}
+      @user     = DoubleDog.db.create_user(@args)
+    end
+
+    let(:result) { @use_case.run(@args) }
+
     it "requires a non-nil username" do
-      use_case = DoubleDog::SignIn.new
-      result = use_case.run(username: nil)
+      @args = {username: nil}
 
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:nil_username)
     end
 
     it "requires a non-blank username" do
-      use_case = DoubleDog::SignIn.new
-      result = use_case.run(username: '')
+      @args = {username: ''}
 
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:blank_username)
     end
 
     it "it requires a non-nil password" do
-      use_case = DoubleDog::SignIn.new
-      result = use_case.run(username: 'bob_man', password: nil)
+      @args = {username: 'username', password: nil}
 
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:nil_password)
     end
 
     it "it requires a non-blank password" do
-      use_case = DoubleDog::SignIn.new
-      result = use_case.run(username: 'malice_alice', password: '')
+      @args = {username: 'username', password: ''}
 
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:blank_password)
     end
 
     it "requires the username to exist" do
-      use_case = DoubleDog::SignIn.new
-      result = use_case.run(:username => "doesn't exist", :password => "doesn't exist")
+      @args = {username: 'invalid', password: 'password'}
 
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:no_such_user)
     end
 
-    it "requires the password to match the username" do
-      user = DoubleDog.db.create_user(username: 'sally', password: 'brownie')
-      use_case = DoubleDog::SignIn.new
-      result = use_case.run(:username => user.username, :password => 'pillsbury')
+    it "requires a valid password and username combination" do
+      @args = {username: @user.username, password: 'invalid'}
 
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:invalid_password)
     end
-  end
 
-  it "creates a sessions for the user" do
-    user = DoubleDog.db.create_user(username: 'alice', password: 'beer')
-    use_case = DoubleDog::SignIn.new
-    result = use_case.run(:username => user.username, password: 'beer')
+    it "creates a sessions for the user" do
+      @args = {username: @user.username, password: 'valid_password'}
 
-    expect(result[:success?]).to eq(true)
+      expect(result[:success?]).to eq(true)
+      expect(result[:session_id]).to_not be_nil
+    end
 
-    session_id = result[:session_id]
+    it "retrieves a user from the created session" do
+      @args = {username: @user.username, password: 'valid_password'}
 
-    expect(user.username).to eq('alice')
-    expect(session_id).to_not be_nil
-  end
+      expect(result[:success?]).to eq(true)
 
-  it "retrieves a user from the created session" do
-    user = DoubleDog.db.create_user(username: 'bob', password: 'pass12')
-    use_case = DoubleDog::SignIn.new
-    result = use_case.run(:username => user.username, password: 'pass12')
+      user = result[:user]
+      expect(user.username).to eq(@user.username)
+    end
 
-    expect(result[:success?]).to eq(true)
-
-    user = result[:user]
-    expect(user.username).to eq('bob')
   end
 end
